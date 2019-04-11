f417e2972af1b84c5d635956f2a65333e5410199
ssx <1554816265@qq.com>
2019-04-11 18:41:53 +0800

    test

--- src/class.js
+++ src/class.js
@@ -1,5 +1,5 @@
 
-class MyApp{
+class Person{
     constructor(age) {
         this.name='ssx'
         this.age = age      
@@ -8,5 +8,27 @@ class MyApp{
         console.log(`hello ${this.name}, I am ${this.age} years old`)
     }
 }
-app = new MyApp(18)
-app.sayHello()
+// app = new Person(18)
+// app.sayHello()
+
+class Student extends Person{
+    constructor(age,like){
+        super(age)
+        this.like = like
+    }
+
+    getLike(){
+        return this.like
+    }
+
+    setLike(like){
+        this.like  = like
+    }
+
+}
+
+var s  = new Student(18,'吃东西')
+s.sayHello()
+console.log(s.getLike())
+s.setLike('读书')
+console.log(s.getLike())
